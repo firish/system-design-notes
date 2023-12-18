79e7c0a4d740ed46563162290d20d0389c8f47e3
Rishi Gulati <rishisg2@Rishis-MacBook-Air-3.local>
2023-12-18 20:36:15 +0530

    restructure

---
+++ SOLID/2_ai_engine.py
@@ -0,0 +1,8 @@
+# The objective
+# Create a turn-taking AI Engine
+# This AI engine should be able to play any turn-taking two player game
+# Like Chess, Othello, Tic-Tac-Toe, 
+# It is a truly open-ended question with hazy requirements at best
+
+class Engine:
+    
